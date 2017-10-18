1e2cb2ba2f9e8ab6bcd19b2a67f44d20dbac87f9
Yawuar Serna Delgado <yawuarsernadelgado@hotmail.be>
2017-10-18 23:29:08 +0200

    added coucher component

--- front-end/src/app/name/name.component.ts
+++ front-end/src/app/name/name.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-name',
   templateUrl: './name.component.html',
-  styleUrls: ['./name.component.sass']
+  styleUrls: ['./name.component.scss']
 })
 export class NameComponent implements OnInit {
 
