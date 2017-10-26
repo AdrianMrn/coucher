b21b1cf533bf05d7acbbade1bfa6d570d5b4fd7a
Yawuar Serna Delgado <yawuarsernadelgado@hotmail.be>
2017-10-27 00:32:58 +0200

    added module of materialize

--- front-end/src/app/user/user.component.ts
+++ front-end/src/app/user/user.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter } from '@angular/core';
+import { MaterializeAction } from 'angular2-materialize';
 
 @Component({
   selector: 'app-user',
@@ -10,14 +11,15 @@ export class UserComponent implements OnInit {
   constructor() { 
   }
 
-  toggleChild(){
-	  this.showVar = !this.showVar;
+  modalActions = new EventEmitter<string|MaterializeAction>();
+  openModal(){
+	  this.modalActions.emit({action:"modal",params:['open']});
+  }
+
+  closeModal() {
+    this.modalActions.emit({action:"modal",params:['close']});
   }
 
   ngOnInit() {
-  }
-
-  event() {
-    this.showVar = !this.showVar;
   }
 }
